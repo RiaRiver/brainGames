d05317fba57055f426e5dc2b8d81836825cc39bd
RiaRiver <riria.river@gmail.com>
2021-07-02 00:41:25 +0300

    refactor tests

--- __tests__/gcd.test.js
+++ __tests__/gcd.test.js
@@ -1,7 +1,7 @@
-import { getGcd } from '../games/gcd-game.js';
+import { getGcd } from '../src/games/gcd-game.js';
 
 describe('greatest common divisor', () => {
-  it('common cases', () => {
+  test('common cases', () => {
     expect(getGcd(13, 26)).toBe(13);
     expect(getGcd(14, 21)).toBe(7);
     expect(getGcd(21, 35)).toBe(7);
@@ -11,7 +11,7 @@ describe('greatest common divisor', () => {
     expect(getGcd(40, 10)).toBe(10);
   });
 
-  it('unique cases', () => {
+  test('unique cases', () => {
     expect(getGcd(0, 0)).toBeNaN();
     expect(getGcd(0, 21)).toBe(21);
     expect(getGcd(25, 0)).toBe(25);
